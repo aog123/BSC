7b77166938fb0defd3410383d638bb694841b805
Jaydeep Rajpurohit <jaydeeppurohit1996@gmail.com>
2021-11-24 20:50:09 +0530

    init

---
+++ scripts/eth-bsc-bridge.js
@@ -0,0 +1,49 @@
+const Web3 = require('web3');
+const BridgeEth = require('../build/contracts/BridgeEth.json');
+const BridgeBsc = require('../build/contracts/BridgeBsc.json');
+
+const web3Eth = new Web3('wss://ropsten.infura.io/ws/v3/ffdd55345da6440fb4fff0ce84074f38');
+const web3Bsc = new Web3('https://data-seed-prebsc-1-s2.binance.org:8545/');
+const adminPrivKey = '4107c052723f1a92e6a6f6fd81d6b20d75578637584a4c72808f1d44be6c473e';
+const { address: admin } = web3Bsc.eth.accounts.wallet.add(adminPrivKey);
+
+const bridgeEth = new web3Eth.eth.Contract(
+  BridgeEth.abi,
+  BridgeEth.networks['3'].address
+);
+
+const bridgeBsc = new web3Bsc.eth.Contract(
+  BridgeBsc.abi,
+  BridgeBsc.networks['97'].address
+);
+
+bridgeEth.events.Transfer(
+  {fromBlock: 0, step: 0}
+)
+.on('data', async event => {
+  const { from, to, amount, date, nonce, signature } = event.returnValues;
+
+  const tx = bridgeBsc.methods.mint(from, to, amount, nonce, signature);
+  const [gasPrice, gasCost] = await Promise.all([
+    web3Bsc.eth.getGasPrice(),
+    tx.estimateGas({from: admin}),
+  ]);
+  const data = tx.encodeABI();
+  const txData = {
+    from: admin,
+    to: bridgeBsc.options.address,
+    data,
+    gas: gasCost,
+    gasPrice
+  };
+  const receipt = await web3Bsc.eth.sendTransaction(txData);
+  console.log(`Transaction hash: ${receipt.transactionHash}`);
+  console.log(`
+    Processed transfer:
+    - from ${from} 
+    - to ${to} 
+    - amount ${amount} tokens
+    - date ${date}
+    - nonce ${nonce}
+  `);
+});
